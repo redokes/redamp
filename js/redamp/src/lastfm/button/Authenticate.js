48459d1d013f88ee851fda2c53c08b0ee5079adf
Jared Lewis <jaredtlewis@gmail.com>
2011-12-05 10:55:19 -0500

    * Fixed lastfm auth

--- js/redamp/src/lastfm/button/Authenticate.js
+++ js/redamp/src/lastfm/button/Authenticate.js
@@ -3,7 +3,6 @@ Ext.define('RedAmp.lastfm.button.Authenticate', {
 	text: 'Authenticate',
 	handler: function(){
 		var key = RedAmp.lastfm.api.Api.key;
-		console.log(window.location.hostname);
-		window.open("http://www.last.fm/api/auth/?api_key=" + key + "&cb=http://redamp/auth/lastfm.php", "_blank","");
+		window.open("http://www.last.fm/api/auth/?api_key=" + key + "&cb=http://" + window.location.hostname + "/auth/lastfm.php", "_blank","");
 	}
 });
